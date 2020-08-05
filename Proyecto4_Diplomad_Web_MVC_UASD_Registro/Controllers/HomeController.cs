f34be9112b91f48a834f9a765051d678f96a5b52
CarlosRPEvertsz <carlosrpevertsz@gmail.com>
2020-08-04 22:28:10 -0400

    Add project files.

---
+++ Proyecto4_Diplomad_Web_MVC_UASD_Registro/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto4_Diplomad_Web_MVC_UASD_Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            Proyecto4_Diplomad_Web_MVC_UASD_Registro.Models.LibroVisitas libro = new Proyecto4_Diplomad_Web_MVC_UASD_Registro.Models.LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+        public ActionResult ListadoVisitas()
+        {
+            Proyecto4_Diplomad_Web_MVC_UASD_Registro.Models.LibroVisitas libro = new Proyecto4_Diplomad_Web_MVC_UASD_Registro.Models.LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+            return View();
+        }
+    }
+}
